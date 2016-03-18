35e87df661033a5adfce80953a02ce63b49d89fd
Christian Fischer <sw-dev@computerlyrik.de>
2016-03-18 11:41:24 +0100

    initial project commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # selenium-spring-boot-example
 Example of packaging a Selenium Test with HTMLLauncher with use of spring-boot
+
+This an example project to test the following issues:
+
+https://github.com/SeleniumHQ/selenium/issues/1840
+https://github.com/SeleniumHQ/selenium/issues/1833
---
+++ src/main/java/de/computerlyrik/selenium/Application.java
@@ -0,0 +1,13 @@
+package de.computerlyrik.selenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+
+}

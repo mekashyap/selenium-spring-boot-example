50d04334d2f03402e9714b8f86244502417d3d22
mekashyap <114917154+mekashyap@users.noreply.github.com>
2022-10-14 20:51:12 +0530

    Update and rename src/main/java/de/computerlyrik/selenium/TestConfig.java to src/main/java/de/computerlyrik/AutomationConstants.java

---
+++ src/main/java/de/computerlyrik/AutomationConstants.java
@@ -0,0 +1,24 @@
+package de.computerlyrik.selenium;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+
+public class AutomationConstants {
+
+	private static final String DEBUG_PORT= "--remote-debugging-port=9222";
+	private static final String CHROME_OPTION_TO_MAXIMIZE_WINDOW="start=maximized";
+	private static final String  NO_SANDBOX="--no-sandbox";
+	private static final String  USERNAME_ID="x-auto-8-input";
+        private static final String  USERNAME_ID="x-auto-9input";
+	   private static final String  LOGIN_URL="https://www.xyz.com";
+	   private static final String  JOB_NAME_XPATH="";
+	  private static final String  MACHINE_NAME_XPATH="";
+	
+
+                                                  
+
+	
+}
